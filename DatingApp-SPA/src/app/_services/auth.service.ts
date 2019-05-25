fbaa2759a206ad26604861df49c215935bc3f1e7
unknown <you@example.com>
2019-05-25 23:28:11 +0530

    Till 44.5 Adding Register Method to AuthService

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -22,4 +22,9 @@ constructor(private http: HttpClient) {}
         })
   );
  }
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register', model);
+}
+
 }
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -21,4 +21,15 @@ export class NavComponent implements OnInit {
         console.log('Failed to login.');
       });
   }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+  // if something is there in token, return true else false
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
 }
